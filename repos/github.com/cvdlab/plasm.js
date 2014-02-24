b1d9ac389e3928130c4e9f3768b490a32c3e9dbf
piaoger-macpro <piaoger@192.168.0.102>
2014-02-24 23:00:14 +0800

    add some new repos

---
+++ repos/github.com/cvdlab/plasm.js
@@ -0,0 +1,25 @@
+{
+    "name":"plasm.js",
+    "description": "JavaScript Programming Language for Solid Modeling.",
+
+    "keywords": [
+        "solid modeling",
+        "language"
+    ],
+
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/cvdlab/plasm.js.git",
+
+        "branches": [
+            "master"
+        ],
+        "tags": [
+        ]
+    },
+
+    "linkes": {
+        "Homepage": "http://cvdlab.github.com/plasm.js",
+        "showcase":"http://cvdlab.github.io/plasm.js/showcase.html"
+    }
+}
