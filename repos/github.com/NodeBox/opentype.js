8156ef033cd10e328c6dc7823239eb47d574fba9
piaoger <piaogerbeta@gmail.com>
2014-07-16 22:57:59 +0800

    add nodebox

---
+++ repos/github.com/NodeBox/opentype.js
@@ -0,0 +1,26 @@
+{
+    "name":"opentype.js",
+    "description": "A JavaScript parser for TrueType and OpenType fonts.",
+
+    "keywords": [
+        "opentype",
+        "truetype",
+        "JavaScript"
+    ],
+
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/nodebox/opentype.js.git",
+
+        "branches": [
+            "master"
+        ],
+        "tags": [
+        ]
+    },
+
+    "linkes": {
+        "Home page": "http://www.emrg.be",
+        "Project Page": "http://nodebox.github.io/opentype.js/"
+    }
+}
