14c8a725fd8630d186e4b02d55159382b348d0f7
solidmcp <piaoger@gmail.com>
2014-11-06 14:22:13 +0800

    add jot-lib ..

---
+++ repos/github.com/gabime/spdlog.cmake
@@ -0,0 +1,23 @@
+{
+    "name":"spdlog",
+    "description": "Super fast c++ logging library.",
+
+    "keywords": [
+        "c++ 11",
+        "logging"
+    ],
+
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/gabime/spdlog.git",
+
+        "branches": [
+            "master"
+        ],
+        "tags": [
+        ]
+    },
+
+    "linkes": {
+    }
+}
