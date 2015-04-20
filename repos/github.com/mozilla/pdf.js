d5199ee9b54577647117eb918b8d03abd864e795
piaoger <piaoger@yahoo.com>
2015-04-21 00:40:14 +0800

    add campskeleton.repo and pdf.js

---
+++ repos/github.com/mozilla/pdf.js
@@ -0,0 +1,24 @@
+{
+    "name":"pdf.js",
+    "description": "PDF reader in javascript",
+
+    "keywords": [
+        "pdf",
+        "javascript"
+    ],
+
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/mozilla/pdf.js.git",
+
+        "branches": [
+            "master"
+        ],
+        "tags": [
+        ]
+    },
+
+    "linkes": {
+
+    }
+}
